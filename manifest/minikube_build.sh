31f59fe5728af3fbe022653fa22d1e86d884bee6
eumhwa <dmaghk1007@naver.com>
2021-12-29 19:35:48 +0900

    update streamlit dev & docker network confirm

--- manifest/minikube_build.sh
+++ manifest/minikube_build.sh
@@ -1,7 +1,7 @@
 eval $(minikube -p minikube docker-env)
 
 cd ../fastapi_app && docker build -t backend:dev .
-docker run -itd -p 8002:8002 --name backend backend:dev
+# docker run -itd -p 8002:8002 --name backend backend:dev
 
 cd ../streamlit_app && docker build -t frontend:dev .
-docker run -itd -p 8000:8000 --name frontend frontend:dev
+# docker run -itd -p 8000:8000 --name frontend frontend:dev
